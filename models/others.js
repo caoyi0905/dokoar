681a5269b1c1b870c5e7b101b32aea0abe699a31
caoyi <caoyi0905@163.com>
2016-01-28 11:41:30 +0800

    2016年1月28日
    完善ContainerInfo中的PortsConfig

--- models/others.js
+++ models/others.js
@@ -22,13 +22,29 @@ exports.formatContainerInfo=function (Info){
 
     Info.startTime=new Date(Info.State.StartedAt).toString();
 
-
     Info.State.Paused=(Info.State.Paused==true)?1:0;
     Info.State.Running=(Info.State.Running==true)?1:0;
 
+    if(Info.HostConfig.Memory!=null)
+        Info.HostConfig.Memory=Info.HostConfig.Memory/1024/1024+' MB';
 
-    Info.HostConfig.Memory=Info.HostConfig.Memory/1024/1024+' MB';
+    if(Info.NetworkSettings.Ports!=null){
+        Info.PortsConfig=[];
+        for(var port in Info.NetworkSettings.Ports){
+            console.log(Info.NetworkSettings.Ports[port])
+            if(Info.NetworkSettings.Ports[port]!=null)
+            {
 
-    console.log(Info)
+                Info.PortsConfig.push('Exposed: '+Info.NetworkSettings.Ports[port][0].HostIp+
+                    ':'+Info.NetworkSettings.Ports[port][0].HostPort+' → '+port);
+            }else{
+                Info.PortsConfig.push('Internal: '+port);
+            }
+        }
+
+
+    }
+
+    console.log(JSON.stringify(Info))
     return Info;
 }
